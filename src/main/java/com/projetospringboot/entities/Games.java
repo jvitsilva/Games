ce4e76e98461f5bddefbf4b66297b51d42bce52b
João Vitor Oliveira da Silva <100168573+jvitsilva@users.noreply.github.com>
2023-07-11 11:35:07 -0300

    Add files via upload

---
+++ src/main/java/com/projetospringboot/entities/Games.java
@@ -0,0 +1,13 @@
+package com.projetospringboot.entities;
+
+public class Games {
+
+    private Long id;
+    private String title;
+    private int year;
+    private String genre;
+    private
+    private
+    private
+    private
+}
